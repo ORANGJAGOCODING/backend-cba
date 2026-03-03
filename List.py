7e8072368810d5f78aae2b13a04950bef7a124d5
ORANGJAGOCODING <pocopon.no2@gmail.com>
2026-03-03 22:52:51 +0700

    Commit pertama

---
+++ List.py
@@ -0,0 +1,18 @@
+a = {
+    "username": "labfti",
+    "home": "/home/labfti/",
+    "uid": 500,
+    400: "untar"
+}
+
+print(a)
+u=a["username"]
+d=a["home"]
+print(u)
+print(d)
+
+a["username"]="pxl"
+a["home"]="/home/pxl"
+a["shell"]="/usr/bin/bash"
+print(a)
+print ('username' in a)
